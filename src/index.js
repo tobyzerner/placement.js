35772e359e318f2f30a8bb6e8550a5c67ba83210
Toby Zerner <toby.zerner@gmail.com>
2019-10-03 16:43:53 +0930

    Allow a custom bounding rect to be specified (defaults to window dimensions)
    Also fix some margin calculations

--- src/index.js
+++ src/index.js
@@ -24,6 +24,20 @@ export default function(anchorRect, popup, side = 'bottom', align = 'center', op
     right: anchorRect.left,
   }, anchorRect);
 
+  const boundRect = {
+    top: 0,
+    left: 0,
+    bottom: window.innerHeight,
+    right: window.innerWidth
+  };
+
+  if (options.bound) {
+    if (options.bound.getBoundingClientRect) {
+      options.bound = options.bound.getBoundingClientRect();
+    }
+    Object.assign(boundRect, options.bound);
+  }
+
   const popupStyle = getComputedStyle(popup);
 
   const primary = {};
@@ -35,15 +49,25 @@ export default function(anchorRect, popup, side = 'bottom', align = 'center', op
   }
 
   popup.style.position = fixed ? 'fixed' : 'absolute';
+  popup.style.maxWidth = '';
+  popup.style.maxHeight = '';
+
+  // Constrain the maximum size of the popup along the secondary axis.
+  const secondaryMarginBefore = parseInt(popupStyle[secondary.marginBefore]);
+  const secondaryMarginAfter = parseInt(popupStyle[secondary.marginAfter]);
+  const secondaryMargin = secondaryMarginBefore + secondaryMarginAfter;
+  const secondaryMaxSize = boundRect[secondary.after] - boundRect[secondary.before] - secondaryMargin;
+
+  if (popup[secondary.offsetSize] > secondaryMaxSize) {
+    popup.style[secondary.maxSize] = secondaryMaxSize + 'px';
+  }
 
   // Calculate the available room on either side of the anchor element. If
   // the size of the popup is more than is available on the given side, then we
   // will switch to the side with more room.
-  popup.style.maxWidth = '';
-  popup.style.maxHeight = '';
-
-  const roomBefore = anchorRect[primary.before];
-  const roomAfter = document.documentElement[primary.clientSize] - anchorRect[primary.after];
+  const margin = parseInt(popupStyle[primary.marginBefore]) + parseInt(popupStyle[primary.marginAfter]);
+  const roomBefore = anchorRect[primary.before] - boundRect[primary.before] - margin;
+  const roomAfter = boundRect[primary.after] - anchorRect[primary.after] - margin;
 
   if ((side === primary.before && popup[primary.offsetSize] > roomBefore)
     || (side === primary.after && popup[primary.offsetSize] > roomAfter)) {
@@ -52,8 +76,7 @@ export default function(anchorRect, popup, side = 'bottom', align = 'center', op
 
   // If the size of the popup exceeds the room available on this side, then
   // we will give the popup an explicit size so that it doesn't go off-screen.
-  const margin = parseInt(popupStyle[primary.marginBefore]) + parseInt(popupStyle[primary.marginAfter]);
-  const maxSize = (side === primary.before ? roomBefore : roomAfter) - margin;
+  const maxSize = side === primary.before ? roomBefore : roomAfter;
 
   if (popup[primary.offsetSize] > maxSize) {
     popup.style[primary.maxSize] = maxSize + 'px';
@@ -65,46 +88,37 @@ export default function(anchorRect, popup, side = 'bottom', align = 'center', op
   const scrollOffset = fixed ? 0 : window[primary.scrollOffset];
 
   if (side === primary.before) { // top or left
-    popup.style[primary.before] = 'auto';
-    popup.style[primary.after] = (-scrollOffset + document.documentElement[primary.clientSize] - anchorRect[primary.before]) + 'px';
+    popup.style[primary.before] = (scrollOffset + anchorRect[primary.before] - popup[primary.offsetSize] - margin) + 'px';
+    popup.style[primary.after] = 'auto';
   } else { // bottom or right
     popup.style[primary.before] = (scrollOffset + anchorRect[primary.after]) + 'px';
     popup.style[primary.after] = 'auto';
   }
 
   // Set the position of the popup element along the secondary axis.
-  const secondaryMargin = parseInt(popupStyle[secondary.marginAfter]) + parseInt(popupStyle[secondary.marginBefore]);
-  const secondaryMaxSize = document.documentElement[secondary.clientSize] - secondaryMargin;
-
-  if (popup[secondary.offsetSize] > secondaryMaxSize) {
-    popup.style[secondary.maxSize] = secondaryMaxSize + 'px';
-  }
-
   const secondaryScrollOffset = fixed ? 0 : window[secondary.scrollOffset];
 
   const boundPos = pos => {
     return Math.max(
-        0,
-        Math.min(
-            pos,
-            document.documentElement[secondary.clientSize] - popup[secondary.offsetSize] - secondaryMargin
-        )
+      boundRect[secondary.before],
+      Math.min(pos, boundRect[secondary.after] - popup[secondary.offsetSize] - secondaryMargin)
     );
   };
 
   switch (align) {
     case 'start':
-      popup.style[secondary.before] = (secondaryScrollOffset + boundPos(anchorRect[secondary.before])) + 'px';
+      popup.style[secondary.before] = (secondaryScrollOffset + boundPos(anchorRect[secondary.before] - secondaryMarginBefore)) + 'px';
       popup.style[secondary.after] = 'auto';
       break;
 
     case 'end':
       popup.style[secondary.before] = 'auto';
-      popup.style[secondary.after] = (secondaryScrollOffset + boundPos(document.documentElement[secondary.clientSize] - anchorRect[secondary.after])) + 'px';
+      popup.style[secondary.after] = (secondaryScrollOffset + boundPos(document.documentElement[secondary.clientSize] - anchorRect[secondary.after] - secondaryMarginAfter)) + 'px';
       break;
 
     default: // center
-      popup.style[secondary.before] = (secondaryScrollOffset + boundPos(anchorRect[secondary.before] + anchor[secondary.offsetSize] / 2 - popup[secondary.offsetSize] / 2)) + 'px';
+      const anchorSize = anchorRect[secondary.after] - anchorRect[secondary.before];
+      popup.style[secondary.before] = (secondaryScrollOffset + boundPos(anchorRect[secondary.before] + anchorSize / 2 - popup[secondary.offsetSize] / 2 - secondaryMarginBefore)) + 'px';
       popup.style[secondary.after] = 'auto';
   }
 
